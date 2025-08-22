f3c8a0cce513da13da7abf0b51758c0e63f6aa2e
cupapih <seins3002@gmail.com>
2025-08-23 01:17:18 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function decodeUrl(encodedUrl) {
     const hostname = url.hostname;
     const targetDomains = ['videy.co', 'mediafire', 'terabox'];
     const isTarget = targetDomains.some(domain => hostname.includes(domain));
-    return isTarget ? `https://savelink-nekopara.vercel.app/?url=${encodedUrl}` : decoded;
+    return isTarget ? `https://nekopara-savelink.netlify.app/?url=${encodedUrl}` : decoded;
   } catch (e) {
     console.error("Gagal decode:", encodedUrl);
     return "#";
